2238320e7630133b83ff8511efcb7191f4788514
soogineer <ysooroh54@gmail.com>
2023-05-01 12:24:15 +0900

    Controller 파일로 로직 분리

--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const productController = require("./controller/products");
 
-router.get('/', (req, res) => {
-    res.send("안녕하세요")
-})
+router.get('/', productController.hello)
 
 module.exports = router;
