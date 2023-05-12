45c063c3078471af9890bba299c0bfa6bfec3d30
soogineer <ysooroh54@gmail.com>
2023-05-12 13:00:02 +0900

    created response results back

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -33,6 +33,10 @@ describe("Product Controller Create", () => {
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy();
   })
-
+  it("should return json body in resposne", () => {
+    productModel.create.mockReturnValue(newProduct)
+    productController.createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newProduct)
+  })
 
 });
