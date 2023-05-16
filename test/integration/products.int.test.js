fbb9eb95232ba53c32542703af3e4291dfab7ff0
soogineer <ysooroh54@gmail.com>
2023-05-16 14:56:09 +0900

    Express Error Handling

--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -16,8 +16,6 @@ it("should return 500 on POST /api/products", async () => {
     .post('/api/products')
     .send({ name: "soogineer" })
     expect(response.statusCode).toBe(500);
-
     console.log('response.body', response.body)
-
     expect(response.body).toStrictEqual({ message: "Product validation failed: description: Path `description` is required."})
 });
