95d406a1ac3b10539116e9fe957a06c1bea96dac
soogineer <ysooroh54@gmail.com>
2023-05-16 14:15:55 +0900

    fixed error codes and improved

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const express = require('express');
+const express = require("express");
 
-const PORT = 5000;
+const PORT = 5500;
 
 const app = express();
-const productRoutes = require('./routes');
-const mongoose = require('mongoose');
+const productRoutes = require("./routes");
+const mongoose = require("mongoose");
+app.use(express.json());
 
+mongoose
+  .connect(
+    "mongodb+srv://soogineer:qwer1234@cluster0.kxtder7.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected.."))
+  .catch(err => console.log(err));
 
-mongoose.connect('mongodb+srv://soogineer:qwer1234@cluster0.kxtder7.mongodb.net/test',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(()=> console.log('MongoDB Connected..'))
-  .catch(err => console.log(err))
-
-app.use("/api/products", productRoutes)
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use("/api/products", productRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(PORT);
-console.log(`Running on port ${PORT}`)
+console.log(`Running on port ${PORT}`);
 
 module.exports = app;
