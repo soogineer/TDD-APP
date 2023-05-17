e5eabcf8dd6cc235676a853d0cbc3ab8aa47f336
soogineer <ysooroh54@gmail.com>
2023-05-17 14:42:00 +0900

    created unit test code for getProducts

--- controller/products.js
+++ controller/products.js
@@ -10,3 +10,6 @@ exports.createProduct = async (req, res, next) => {
   }
 };
 
+exports.getProducts = async(req, res, next) => {
+  
+}
--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -1,21 +1,23 @@
-const request = require('supertest');
-const app = require('../../server');
-const newProduct = require('../data/new-product.json');
+const request = require("supertest");
+const app = require("../../server");
+const newProduct = require("../data/new-product.json");
 
-it("POST /api/products", async() => {
-    const response = await request(app)
-      .post("/api/products") 
-      .send(newProduct)
-    expect(response.statusCode).toBe(201)
-    expect(response.body.name).toBe(newProduct.name)
-    expect(response.body.description).toBe(newProduct.description)
-})
+it("POST /api/products", async () => {
+  const response = await request(app).post("/api/products").send(newProduct);
+  expect(response.statusCode).toBe(201);
+  expect(response.body.name).toBe(newProduct.name);
+  expect(response.body.description).toBe(newProduct.description);
+});
 
 it("should return 500 on POST /api/products", async () => {
-    const response = await request(app)
-    .post('/api/products')
-    .send({ name: "soogineer" })
-    expect(response.statusCode).toBe(500);
-    console.log('response.body', response.body)
-    expect(response.body).toStrictEqual({ message: "Product validation failed: description: Path `description` is required."})
+  const response = await request(app)
+    .post("/api/products")
+    .send({ name: "soogineer" });
+  expect(response.statusCode).toBe(500);
+  expect(response.body).toStrictEqual({
+    message:
+      "Product validation failed: description: Path `description` is required.",
+  });
 });
+
+
