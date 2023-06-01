1f48601c213f147b1256541a4ac4a58ce9247626
soogineer <ysooroh54@gmail.com>
2023-06-01 23:15:44 +0900

    created intergration test codes for getProductById

--- routes.js
+++ routes.js
@@ -4,5 +4,5 @@ const productController = require("./controller/products");
 
 router.post('/', productController.createProduct);
 router.get('/', productController.getProducts);
-
+router.get('/:productId', productController.getProductById)
 module.exports = router;
