3d603b743b6821e79db523439ad8328f77bb5692
soogineer <ysooroh54@gmail.com>
2023-06-08 13:29:04 +0900

    created int test codes for updateProduct

--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ const productController = require("./controller/products");
 router.post('/', productController.createProduct);
 router.get('/', productController.getProducts);
 router.get('/:productId', productController.getProductById)
+router.put('/:productId', productController.updateProduct)
+
+
 module.exports = router;
--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -22,22 +22,32 @@ it("should return 500 on POST /api/products", async () => {
   });
 });
 it("GET /api/products", async () => {
-  const response = await request(app).get('/api/products');
+  const response = await request(app).get("/api/products");
   expect(response.statusCode).toBe(200);
   expect(Array.isArray(response.body)).toBeTruthy();
   expect(response.body[0].name).toBeDefined();
   expect(response.body[0].description).toBeDefined();
-  firstProduct = response.body[0]
-})
+  firstProduct = response.body[0];
+});
 
 it("GET /api/product/:productId", async () => {
-  const response = await request(app).get('/api/products/' + firstProduct._id)
+  const response = await request(app).get("/api/products/" + firstProduct._id);
   expect(response.statusCode).toBe(200);
   expect(response.body.name).toBe(firstProduct.name);
   expect(response.body.description).toBe(firstProduct.description);
-})
+});
 
-it("GET id doesnt exist /api/products/:productId", async() => {
-    const response = await request(app).get('/api/products/4djfk5dfdfdfk3j')
-    expect(response.statusCode).toBe(500);
-} )
+it("GET id doesnt exist /api/products/:productId", async () => {
+  const response = await request(app).get("/api/products/4djfk5dfdfdfk3j");
+  expect(response.statusCode).toBe(500);
+});
+
+it("PUT /api/products", async () => {
+    const res = await request(app)
+        .put("/api/products" + firstProduct._id)
+        .send({name:"updated name", description: "updated description"});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.name).toBe("updated name");
+    expect(res.body.description).toBe("updated description");
+
+});
