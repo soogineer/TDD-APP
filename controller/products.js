c9654202d11ba6266debdd3499afa982aad66e9f
soogineer <ysooroh54@gmail.com>
2023-06-12 15:40:54 +0900

    fixexd unit test codes when data is deleted and an error occured

--- controller/products.js
+++ controller/products.js
@@ -51,11 +51,15 @@ exports.updateProduct = async (req, res, next) => {
 };
 
 exports.deleteProduct = async(req, res, next) => {
-  const deletedProduct = await Product.findByIdAndDelete(req.params.productId);
-  if(deletedProduct) {
-    res.status(200).json(deletedProduct);
-  } else {
-    res.status(404).send();
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.productId);
+    if(deletedProduct) {
+      res.status(200).json(deletedProduct);
+    } else {
+      res.status(404).send();
+    }
+  } catch(err) {
+    next(err);
   }
   
 };
